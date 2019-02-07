e7bda1e3f2094121634a74bf8714d3dec638fd32
Duncan Lowther <2402789L@student.gla.ac.uk>
2019-02-07 19:15:20 +0000

    Mid chapter 9

--- rango/urls.py
+++ rango/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
             views.add_page, name='add_page'),
         url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
             views.show_category, name='show_category'),
+        url(r'^login/$', views.user_login, name='login'),
+        url(r'^register/$', views.register, name='register'),
 ]
 
